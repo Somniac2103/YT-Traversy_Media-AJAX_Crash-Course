6790fbbd80ced5b11b2b04d9620b9e1b20934e01
Somniac2103 <salmon.barry1@gmail.com>
2023-10-09 23:39:14 +0700

    Continue with AJAX training

---
+++ process.php
@@ -0,0 +1,13 @@
+<?php
+
+echo 'Processing...';
+
+//check for POST variable
+if (isset($_POST['name'])) {
+  echo 'POST: Your name is ' . $_POST['name'];
+}
+
+//check for GET variable
+if (isset($_GET['name'])) {
+  echo 'GET: Your name is ' . $_GET['name'];
+}
